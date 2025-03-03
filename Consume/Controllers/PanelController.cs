82f4f1301b99b16cb76c5678c72edd86b1124045
Bhakti Sojitra <sojitrabhakti03@gmail.com>
2025-03-03 06:11:34 +0530

    Add files via upload

---
+++ Consume/Controllers/PanelController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Consume.Controllers
+{
+    public class PanelController : Controller
+    {
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+        public IActionResult User()
+        {
+            return View();
+        }
+    }
+}
